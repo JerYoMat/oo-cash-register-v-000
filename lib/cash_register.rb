51304141b95c7da5bb3f97ce030f25f808f61922
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-04 15:03:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   
   def apply_discount
     @total = @total - (@total * discount/100) 
-    puts "Success"
+    puts "After the discount, the total comes to ${total}."
   end 
   
 
